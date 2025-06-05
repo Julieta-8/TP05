c67f682f03d213ca7f1675d8e98a26840c1b479c
JULIETA USOZ <48799592@ort>
2025-06-05 15:19:05 -0300

    Clase 5/6

---
+++ Models/Juego.cs
@@ -0,0 +1,19 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Actividad4.Models;
+
+public class Sala_Escape_05
+{
+    string[] Claves = new string[4];
+
+    public Sala_Escape_05()
+    {
+        Claves[0] = "HONOR";
+        Claves[1] = "SABIDURIA";
+        Claves[2] = "LEALTAD";
+        Claves[3] = "ASTUCIA";
+        Claves[4] = "674976";
+       Claves[5] = "SOMBRERO";
+       Claves[6] = "43421";
+    }
+}
